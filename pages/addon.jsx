565685f109125b80b316ac348c4f69e66be99d2c
neolectron <neolectron@codinglab.io>
2021-02-27 21:19:16 +0100

    fix(indexPage): fix button reversed on hover

--- pages/addon.jsx
+++ pages/addon.jsx
@@ -15,7 +15,6 @@ const AddonPage = () => (
             <div className="flex-grow text-gray-600">(brave également)</div>
             <Button
               asAnchor
-              reversed
               href="https://chrome.google.com/webstore/detail/sardalert/elnpfaoipdfdhikjacbpcfhpnehjjaii"
               target="_blank"
               text="Télécharger !"
@@ -32,7 +31,6 @@ const AddonPage = () => (
             <div className="text-xl flex-grow">Sardalert - Firefox</div>
             <Button
               asAnchor
-              reversed
               href="https://addons.mozilla.org/en-US/firefox/addon/sardalert/"
               target="_blank"
               text="Télécharger !"
